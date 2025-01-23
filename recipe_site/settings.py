03cf5944191ad754136cccd0221ea5a818e4204f
David Cuevas Diaz <davidcuevasdiaz@Davids-Laptop.local>
2025-01-23 11:55:14 +0000

    Add new database

--- recipe_site/settings.py
+++ recipe_site/settings.py
@@ -77,12 +77,10 @@ WSGI_APPLICATION = 'recipe_site.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True  # Add this if you're using SSL for production database (common with Render).
-    )
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
